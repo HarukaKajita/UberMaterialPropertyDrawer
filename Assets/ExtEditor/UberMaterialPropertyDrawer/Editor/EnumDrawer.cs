d5b710dd35dd29a9e541b4f6b45d02ca4e2fa53c
HarukaKajita <kajitaj63b3@gmail.com>
2023-10-07 02:21:45 +0900

    EnumDrawerをUnity実装に合わせて修正

--- Assets/ExtEditor/UberMaterialPropertyDrawer/Editor/EnumDrawer.cs
+++ Assets/ExtEditor/UberMaterialPropertyDrawer/Editor/EnumDrawer.cs
@@ -9,7 +9,7 @@ public class EnumDrawer : MaterialPropertyDrawer
 {
     private readonly string _groupName = "";
     private readonly GUIContent[] names;
-    private readonly float[] values;
+    private readonly int[] values;
     public EnumDrawer(string groupName, string enumName)
     {
         this._groupName = groupName;
@@ -17,17 +17,16 @@ public class EnumDrawer : MaterialPropertyDrawer
         var loadedTypes = TypeCache.GetTypesDerivedFrom(typeof(Enum));
         try
         {
-            Debug.Log(loadedTypes.Count + enumName);
             var enumType = loadedTypes.FirstOrDefault(x => x.Name == enumName || x.FullName == enumName);
             var enumNames = Enum.GetNames(enumType);
             this.names = new GUIContent[enumNames.Length];
             for (int i = 0; i < enumNames.Length; ++i)
                 this.names[i] = new GUIContent(enumNames[i]);
+
             var enumVals = Enum.GetValues(enumType);
-            values = new float[enumVals.Length];
+            values = new int[enumVals.Length];
             for (var i = 0; i < enumVals.Length; ++i)
-                values[i] = (int)enumVals.GetValue(i);//Arrayの要素をfloatにするとこける
-            
+                values[i] = (int)enumVals.GetValue(i);
         }
         catch (Exception)
         {
@@ -41,12 +40,16 @@ public class EnumDrawer : MaterialPropertyDrawer
         this._groupName = groupName;
         
         this.names = new GUIContent[enumNames.Length];
-        for (var i = 0; i < enumNames.Length; ++i)
+        for (int i = 0; i < enumNames.Length; ++i)
             this.names[i] = new GUIContent(enumNames[i]);
 
-        values = new float[vals.Length];
-        for (var i = 0; i < vals.Length; ++i)
-            values[i] = (float)vals[i];
+        values = new int[vals.Length];
+        for (int i = 0; i < vals.Length; ++i)
+            values[i] = (int)vals[i];
+    }
+    static bool IsPropertyTypeSuitable(MaterialProperty prop)
+    {
+        return prop.type == MaterialProperty.PropType.Float || prop.type == MaterialProperty.PropType.Range || prop.type == MaterialProperty.PropType.Int;
     }
     public override float GetPropertyHeight(MaterialProperty prop, string label, MaterialEditor editor)
     {
@@ -60,22 +63,52 @@ public class EnumDrawer : MaterialPropertyDrawer
     {
         if (!UberDrawer.GetGroupExpanded(_groupName))return;
         
-        var propName = ObjectNames.NicifyVariableName(label.text);
-        EditorGUI.showMixedValue = prop.hasMixedValue;
-        var value = prop.floatValue;
-        var selectedIndex = -1;
-        for (var index = 0; index < values.Length; index++)
+        if (!IsPropertyTypeSuitable(prop))
         {
-            var i = values[index];
-            if (i == value)
-            {
-                selectedIndex = index;
-                break;
-            }
+            GUIContent c = new GUIContent("Enum used on a non-float property: " + prop.name);
+            EditorGUI.LabelField(position, c, EditorStyles.helpBox);
+            return;
         }
-        var selIndex = EditorGUI.Popup(position, label, selectedIndex, names);
-        EditorGUI.showMixedValue = false;
-        // prop.intValue = values[selIndex];
-        prop.floatValue = (float)values[selIndex];
+
+        if (prop.type == MaterialProperty.PropType.Float || prop.type == MaterialProperty.PropType.Range)
+        {
+            EditorGUI.showMixedValue = prop.hasMixedValue;
+
+            var value = (int)prop.floatValue;
+            int selectedIndex = -1;
+            for (var index = 0; index < values.Length; index++)
+            {
+                var i = values[index];
+                if (i == value)
+                {
+                    selectedIndex = index;
+                    break;
+                }
+            }
+
+            var selIndex = EditorGUI.Popup(position, label, selectedIndex, names);
+            EditorGUI.showMixedValue = false;
+            prop.floatValue = (float)values[selIndex];
+        }
+        else
+        {
+            EditorGUI.showMixedValue = prop.hasMixedValue;
+
+            var value = prop.intValue;
+            int selectedIndex = -1;
+            for (var index = 0; index < values.Length; index++)
+            {
+                var i = values[index];
+                if (i == value)
+                {
+                    selectedIndex = index;
+                    break;
+                }
+            }
+
+            var selIndex = EditorGUI.Popup(position, label, selectedIndex, names);
+            EditorGUI.showMixedValue = false;
+            prop.intValue = values[selIndex];
+        }
     }
 }
