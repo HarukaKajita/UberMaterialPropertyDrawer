81dbf8cf9d8f587f7ae83e3b62987614a23ce1e5
HarukaKajita <kajitaj63b3@gmail.com>
2026-02-02 21:32:27 +0900

    UberToggleのベクトル化対応

--- Assets/UberMaterialPropertyDrawer/Editor/UberToggleDrawer.cs
+++ Assets/UberMaterialPropertyDrawer/Editor/UberToggleDrawer.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Linq;
 using UnityEditor;
 using UnityEngine;
 
@@ -5,12 +7,18 @@ namespace ExtEditor.UberMaterialPropertyDrawer
 {
     public class UberToggleDrawer : UberDrawerBase
     {
+        private int _dimention = 1;
         public UberToggleDrawer() : base()
         {
         }
 
         public UberToggleDrawer(string groupName) : base(groupName)
         {
+        }
+        
+        public UberToggleDrawer(string groupName, float dim) : base(groupName)
+        {
+            _dimention = (int)Math.Clamp(dim,1,4);
         }
 
         public override float GetPropertyHeight(MaterialProperty prop, string label, MaterialEditor editor)
@@ -22,22 +30,83 @@ namespace ExtEditor.UberMaterialPropertyDrawer
         public override void OnGUI(Rect position, MaterialProperty prop, GUIContent label, MaterialEditor editor)
         {
             if (!IsVisibleInGroup(editor)) return;
-            
-            MaterialEditor.BeginProperty(position, prop);
-            EditorGUI.BeginChangeCheck();
-            EditorGUI.showMixedValue = prop.hasMixedValue;
 
-            var propName = ObjectNames.NicifyVariableName(label.text);
-            var toggleValue = Util.GetAsBool(prop);
-            toggleValue = EditorGUI.Toggle(position, propName, toggleValue);
-            
-            EditorGUI.showMixedValue = false;
-            if (EditorGUI.EndChangeCheck())
+            var propName = label.text;
+            if (_dimention == 1)
             {
-                editor.RegisterPropertyChangeUndo(propName);
-                Util.SetBool(prop, toggleValue);
+                MaterialEditor.BeginProperty(position, prop);
+                EditorGUI.BeginChangeCheck();
+                EditorGUI.showMixedValue = prop.hasMixedValue;
+
+                
+                var toggleValue = Util.GetAsBool(prop);
+                toggleValue = EditorGUI.Toggle(position, propName, toggleValue);    
+            
+                EditorGUI.showMixedValue = false;
+                if (EditorGUI.EndChangeCheck())
+                {
+                    editor.RegisterPropertyChangeUndo(propName);
+                    Util.SetBool(prop, toggleValue);
+                }
+                MaterialEditor.EndProperty();
             }
-            MaterialEditor.EndProperty();
+            else
+            {
+                var tmpWidth = EditorGUIUtility.labelWidth;
+                var labelWidth = position.width;
+                var toggleLabelWidth = 15;
+                var toggleWidth = 20;
+                var spaceWidth = 5;
+                var fieldWidth = (toggleLabelWidth + toggleWidth + spaceWidth) * 4;
+                var leftMargin = 15f;
+                labelWidth -= fieldWidth + leftMargin;
+
+                
+                EditorGUIUtility.labelWidth = labelWidth;
+                var labelRect = new Rect(position.x, position.y, labelWidth , position.height);
+                EditorGUI.PrefixLabel(labelRect, label);
+                
+                var toggleRect = new Rect(
+                    position.x + labelWidth,
+                    position.y,
+                    toggleLabelWidth+toggleWidth+spaceWidth,
+                    position.height);
+                
+                var materials = prop.targets.Cast<Material>().ToArray();
+                
+                var componentLabel = new[] { "X", "Y", "Z", "W" };
+                for (var dim = 0; dim < _dimention; dim++)
+                {
+                    EditorGUI.BeginChangeCheck();
+                    var vectors = materials.Select(m=>m.GetVector(propName)).ToArray();
+                    var floats = vectors.Select(v => v[dim]).ToArray();
+                    var bools = floats.Select(f => f != 0).ToArray();
+                    var headElementBool = bools[0];
+                    var hasMixedValue = bools.Any(b => b != headElementBool);
+                    EditorGUI.showMixedValue = hasMixedValue;
+
+                    var toggleValue = headElementBool;
+                    EditorGUI.LabelField(toggleRect,componentLabel[dim]);
+                    toggleRect.x += toggleLabelWidth;
+                    toggleValue = EditorGUI.Toggle(toggleRect,"", headElementBool);
+                    toggleRect.x += toggleWidth;
+                    toggleRect.x += spaceWidth;
+                    
+                    EditorGUI.showMixedValue = false;
+                    if (EditorGUI.EndChangeCheck())
+                    {
+                        editor.RegisterPropertyChangeUndo(propName);
+                        foreach (var mat in materials)
+                        {
+                            var v = mat.GetVector(propName);
+                            v[dim] = toggleValue ? 1f : 0f;
+                            mat.SetVector(propName, v);
+                        }
+                    }
+                    
+                    EditorGUIUtility.labelWidth = tmpWidth;
+                }
+            }
         }
     }
 }
